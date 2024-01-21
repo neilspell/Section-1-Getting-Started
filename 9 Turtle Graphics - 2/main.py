c1e09bf444f4459c14dddf00cce934db79368592
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:48:38 +0000

    Add files via upload

---
+++ 9 Turtle Graphics - 2/main.py
@@ -0,0 +1,20 @@
+from turtle import *
+
+
+
+forward(100)
+right(90)
+forward(50)
+right(90)
+forward(100)
+right(90)
+forward(50)
+
+
+#forward(100)
+#right(90)
+forward(50)
+right(90)
+forward(100)
+right(90)
+forward(50)
