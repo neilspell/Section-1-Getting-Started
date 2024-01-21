959d1168acea050c820d2d04c039f4fa127c3b56
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:26:42 +0000

    Add files via upload

---
+++ 1 Intro to Printing/main.py
@@ -0,0 +1,11 @@
+# -------------------------
+# Introduction to Printing
+# Date:6/9/22
+# -------------------------
+
+
+# Add comments where necessary, to describe what your code does
+
+# 1. Print hello world
+
+
