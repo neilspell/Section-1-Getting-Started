4ac984b845d0d5cfab13f71e4dc0d0b3f560a4e5
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:30:59 +0000

    Add files via upload

---
+++ 6 Make - Joke & Punchline/main.py
@@ -0,0 +1,16 @@
+# -------------------------------
+# 1.3 Make - Joke & Punchline
+# Date: 14/9/22
+# -------------------------------
+
+# This is a single line comment
+
+import time
+
+
+print("What's red and invisible?\n...No tomatoes")
+
+
+print("Whats red and invisible??")
+time.sleep(3) # Sleep for 3 seconds
+print("...no tomatoes")
