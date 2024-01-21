5a0730bbd4eeff381a0d4c21f72dd5a3d6a53607
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:50:40 +0000

    Update main.py

--- 9 Turtle Graphics - 2/main.py
+++ 9 Turtle Graphics - 2/main.py
@@ -1,20 +1,6 @@
+# --------------------
+# Title:
+# Date:
+# --------------------
 from turtle import *
 
-
-
-forward(100)
-right(90)
-forward(50)
-right(90)
-forward(100)
-right(90)
-forward(50)
-
-
-#forward(100)
-#right(90)
-forward(50)
-right(90)
-forward(100)
-right(90)
-forward(50)
