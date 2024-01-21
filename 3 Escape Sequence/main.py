d2a0aafbdabf95fe81e432238c713bd3db0bf65a
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:28:39 +0000

    Add files via upload

---
+++ 3 Escape Sequence/main.py
@@ -0,0 +1,16 @@
+# ---------------------------
+# Title: 
+# Date: 14/9/22
+# ---------------------------
+
+
+print("In the words of Nelson Mandela, \"Education is the most powerful weapon which we can use to change the world\"")
+
+print("Hello 'Quote' John")
+
+
+# a tab B, new line C
+print("A\tB\nC")
+
+# Double \\ turns to single \
+print("C:\\Users\\johndoe\\Documents\\myfile.txt")
