60a446359b951aa89dce1bc4dd99b659cc69e4a5
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:43:05 +0000

    Add files via upload

---
+++ 8 Turtle Graphics/main.py
@@ -0,0 +1,15 @@
+# Python with Turtle lets you make graphics easily in Python.
+
+
+# import the turtle graphics library
+from turtle import *
+
+
+forward(100) # move forward 100 units
+left(90)     # turn left by 90 degrees
+forward(100)
+right(45)
+forward(50)
+left(90)
+forward(100)
+
