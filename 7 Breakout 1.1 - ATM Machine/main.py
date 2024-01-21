0421deec9d256f35c6754e6703273f0b4595e765
ross-bish <83789503+ross-bish@users.noreply.github.com>
2024-01-21 19:33:06 +0000

    Add files via upload

---
+++ 7 Breakout 1.1 - ATM Machine/main.py
@@ -0,0 +1,26 @@
+# ---------------------------------------
+# Breakout Activity 1.1 - ATM Menu System
+# Date:
+# ---------------------------------------
+
+#The following code creates a user display
+'''
+print("\t|-------------------------------|")
+print("\t|\t LCCS BANK LIMITED\t|")
+print("\t|\t ATM Main Menu\t\t|")
+print("\t|\t\t\t\t\t\t|")
+print("\t|\t1. Balance Enquiry\t|")
+print("\t|\t2. Cash Lodgement\t|")
+print("\t|\t3. Cash Withdrawal\t|")
+print("\t|\t4. Cash Transfer\t|")
+print("\t|\t5. Change PIN\t\t|")
+print("\t|\t6. Other Services\t|")
+print("\t|\t\t\t\t\t\t|")
+print("\t|\t7. Exit\t\t\t\t|")
+print("\t|-------------------------------|")
+print("\t|\t\t\t\t\t\t|")
+print("\t| CHOOSE AN OPTION >> \t\t|")
+print("\t|\t\t\t\t\t\t|")
+print("\t|-------------------------------|")
+print("")
+'''
